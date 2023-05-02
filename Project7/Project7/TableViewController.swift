6e86a8dca3c0a051fcf3d9baf92b3de47c65e4cf
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2023-05-02 16:49:09 -0700

    Add files via upload

---
+++ Project7/Project7/TableViewController.swift
@@ -0,0 +1,78 @@
+//
+//  TableViewController.swift
+//  Project7
+//
+//  Created by Brandon Johns on 4/26/23.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    
+    var petitions = [Petition]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let urlString: String
+
+        if navigationController?.tabBarItem.tag == 0 {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url)
+            {
+                parse(json: data)
+            } else
+            {
+                showError()
+            }
+        } else
+        {
+            showError()
+        }
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    func parse(json: Data)
+    {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json)                                          // Petitions.self = petitions object making an instance
+        {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+            
+        }
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+        
+    }
+   
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let view_controller = Detail_ViewController()
+        view_controller.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(view_controller, animated: true)
+    }
+
+}
